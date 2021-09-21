487782bc6256e0dadf9614167b63db8f6f140099
Pranjay Vats <pranjayvats22062001@gmail.com>
2021-09-21 09:52:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect,useRef } from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const [res,setRes] =useState("");
+    const inputRef=useRef(null);
+
+    useEffect(()=> inputRef.current.focus());
+
+    function clickHandler(e){
+        if(res==="Error"){
+            setRes(e.target.name);
+        }
+        else{    
+            setRes(res+ e.target.name);
+        }    
+    }
+
+    function backspace(){
+        if(res==="Error"){
+             setRes("");
+        } 
+        else if(res){
+           setRes(res.slice(0,-1));
+        }
+
+    }
+    function clear(){
+        setRes("");
+    }
+    function result(){
+        try{
+            setRes(eval(res).toString());
+        }
+        catch(error){
+            setRes("Error");
+        }
+    }
+    return(
+        <div><h1>BasicCalculator.math</h1>
+            <div className="calc-app">
+                
+                <br></br>
+                <form>
+                    <input type="text" value={res}  ref={inputRef} />
+                </form>
+
+                <div className="keypad">
+                    <button id="clear" onClick={clear}>Clear</button>
+                    <button id="backspace" onClick={backspace}> C</button>
+                    <button name="+" onClick={clickHandler}>+</button>
+                    <button name="7" onClick={clickHandler}>7</button>
+                    <button name="8" onClick={clickHandler}>8</button>
+                    <button name="9" onClick={clickHandler}>9</button>
+                    <button name="-" onClick={clickHandler}>-</button>
+                    <button name="4" onClick={clickHandler}>4</button>
+                    <button name="5" onClick={clickHandler}>5</button>
+                    <button name="6" onClick={clickHandler}>6</button>
+                    <button name="*" onClick={clickHandler}>&times;</button>
+                    <button name="1" onClick={clickHandler}>1</button>
+                    <button name="2" onClick={clickHandler}>2</button>
+                    <button name="3" onClick={clickHandler}>3</button>
+                    <button name="/" onClick={clickHandler}>/</button>
+                    <button name="0" onClick={clickHandler}>0</button>
+                    <button name="." onClick={clickHandler}>.</button>
+                    <button id="result" onClick={result}>Result</button>
+                </div>
+            </div>
+        </div>
+        
+        
+    );
 }
-
 export default App;
---
+++ src/Keypad.js
@@ -0,0 +1,5 @@
+// function Keypad(){
+//     return(
+
+//     )
+// }
